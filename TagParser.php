22e8431dd344d6843f6291491fbb581297d58fb1
huangshunzhao <huangshunzhao@wanda.cn>
2016-12-25 01:12:21 +0800

    试出点眉目了

--- TagParser.php
+++ TagParser.php
@@ -58,6 +58,11 @@ class TagParser
     const CHAR_ESCAPE = 92;
 
     /**
+     * /号
+     */
+    const CHAR_CLOSE_TAG = 47;
+
+    /**
      * 修饰符
      */
     const CHAR_FILTER = 124;
@@ -65,12 +70,22 @@ class TagParser
     /**
      * 左中括号
      */
-    const CHAR_LEFT_BRACKET = 91;
+    const CHAR_LEFT_SQUARE_BRACKET = 91;
 
     /**
      * 右中括号
      */
-    const CHAR_RIGHT_BRACKET = 93;
+    const CHAR_RIGHT_SQUARE_BRACKET = 93;
+
+    /**
+     * 左括号
+     */
+    const CHAR_LEFT_BRACKET = 40;
+
+    /**
+     * 右括号
+     */
+    const CHAR_RIGHT_BRACKET = 41;
 
     /**
      * 点
@@ -83,14 +98,77 @@ class TagParser
     const CHAR_COLON = 58;
 
     /**
-     * 关闭标签
+     * 关键字符
      */
-    const TYPE_CLOSE_TAG = 1;
+    const TYPE_KEY_CHAR = 1;
 
     /**
-     * 变量
+     * 数字
      */
-    const TYPE_VAR = 2;
+    const TYPE_NUMBER = 2;
+
+    /**
+     * 标签头
+     */
+    const TYPE_HEAD = 3;
+
+    /**
+     * 字符串
+     */
+    const TYPE_STRING = 4;
+
+    /**
+     * 属性标签
+     */
+    const TYPE_ATTRIBUTE = 5;
+
+    /**
+     * 函数（非自定义插件）
+     */
+    const TYPE_FUNCTION = 6;
+
+    /**
+     * @var array 操作符
+     */
+    private static $operator_char = array(
+        '+' => true,
+        '-' => true,
+        '*' => true,
+        '/' => true,
+        '%' => true,
+        '++' => true,
+        '--' => true,
+        '+=' => true,
+        '-=' => true,
+        '*=' => true,
+        '/=' => true,
+        '&=' => true,
+        '^=' => true,
+        '&' => true,
+        '^' => true,
+        '|' => true,
+        '|=' => true,
+        '%=' => true,
+        '<<=' => true,
+        '>>=' => true,
+        '<<' => true,
+        '>>' => true
+    );
+
+    /**
+     * @var array 关键字符 字符 => 前面是否允许空格|后面是否允许空格
+     */
+    private static $key_char_arr = array(
+        self::CHAR_VAR => 1, //1 代表 前面可以有空格 2 代表后面可以有空格 3 前后都可以有空格
+        self::CHAR_DOT => 0,
+        self::CHAR_COLON => 3,
+        self::CHAR_EQUAL => 3,
+        self::CHAR_FILTER => 3,
+        self::CHAR_LEFT_SQUARE_BRACKET => 2,
+        self::CHAR_RIGHT_SQUARE_BRACKET => 3,
+        self::CHAR_LEFT_BRACKET => 3,
+        self::CHAR_RIGHT_BRACKET => 3
+    );
 
     /**
      * @var string 标准内容
@@ -128,11 +206,6 @@ class TagParser
     private $is_eof = false;
 
     /**
-     * @var int 标签类型
-     */
-    private $tag_type;
-
-    /**
      * @var string 结果
      */
     private $result = '';
@@ -153,25 +226,83 @@ class TagParser
      */
     private function doParse()
     {
-        $first_char = $this->indexChar();
-        if ('$' === $first_char){
-            $this->tag_type = self::TYPE_VAR;
-            $this->popChar();
-            $this->result = $this->parseVar();
-            return;
-        }
-        elseif ('/' === $first_char){
-            $this->tag_type = self::TYPE_CLOSE_TAG;
-            $this->popChar();
-            $this->parseHead();
-            return;
-        }
-        //解析标签头
-        $this->parseHead();
+        $result_arr = array();
+        $type_arr = array();
+        $last_ord = 0;
         while (!$this->is_eof) {
-            $this->parseAttribute();
-            $this->trim();
+            $char = $this->indexChar();
+            $ord = ord($char);
+            //关键字符
+            if (isset(self::$key_char_arr[$ord])) {
+                $result_arr[] = $char;
+                $type_arr[] = self::TYPE_KEY_CHAR;
+                $this->popChar();
+                $opt = self::$key_char_arr[$ord];
+                //前面不允许有空格
+                if (!($opt & 1) && self::CHAR_SPACE === $last_ord) {
+                    throw new TplException('关键字符：' . $char . ' 前面不允许有空格', TplException::TPL_TAG_ERROR);
+                }
+                //后面不允许有空格
+                if (!($opt & 2) && !$this->is_eof && self::CHAR_SPACE === ord($this->indexChar())) {
+                    throw new TplException('关键字符：' . $char . ' 后面不允许有空格', TplException::TPL_TAG_ERROR);
+                }
+            } //普通字符
+            elseif ($this->isNormalChar($ord)) {
+                $result_arr[] = $this->parseNormal();
+                //如果是第一个，表示标签头
+                if (empty($result_arr)) {
+                    $type = self::TYPE_HEAD;
+                } //如果下一个字符是 左括号，表示内置函数
+                elseif (self::CHAR_LEFT_BRACKET === $ord) {
+                    $type = self::TYPE_FUNCTION;
+                } //当成属性
+                else {
+                    $type = self::TYPE_ATTRIBUTE;
+                }
+                $type_arr[] = $type;;
+
+            } //数字
+            elseif ($this->isNumber($ord)) {
+                $result_arr[] = $this->parseNumber();
+                $type_arr[] = self::TYPE_NUMBER;
+            } //引号表示字符串
+            elseif ($ord === self::CHAR_SINGLE_QUOTE || $ord === self::CHAR_QUOTE) {
+                $result_arr[] = $this->parseString($ord);
+                $type_arr[] = self::TYPE_STRING;
+            }//空格
+            else if ($ord === self::CHAR_SPACE) {
+                $this->popChar();
+            } else {
+                throw new TplException('无法解析的字符' . $char, TplException::TPL_TAG_ERROR);
+            }
+            $last_ord = $ord;
         }
+        print_r($result_arr);
+    }
+
+    /**
+     * 普通字符解析
+     * @return string
+     * @throws TplException
+     */
+    private function parseNormal()
+    {
+        $re_str = $this->popChar();
+        while (!$this->is_eof) {
+            $char = $this->indexChar();
+            $ord = ord($char);
+            if (isset(self::$key_char_arr[$ord])) {
+                break;
+            }
+            if (self::CHAR_SPACE === $ord) {
+                break;
+            }
+            if (!$this->isNormalChar($ord) && !$this->isNumber($ord)) {
+                throw new TplException('不支持：' . $re_str . $this->popChar(), TplException::TPL_TAG_ERROR);
+            }
+            $re_str .= $this->popChar();
+        }
+        return $re_str;
     }
 
     /**
@@ -206,18 +337,66 @@ class TagParser
         $char = $this->indexChar();
         $ord = ord($char);
         //变量
-        if (self::CHAR_VAR === $char) {
+        if (self::CHAR_VAR === $ord) {
             return $this->parseVar();
         }
         //单引号 或者 双引号
         if (self::CHAR_QUOTE === $ord || self::CHAR_SINGLE_QUOTE === $ord) {
-            return $this->parseNormal($ord);
+            return $this->parseString($ord);
         }
         $end_char_arr = array(
             self::CHAR_SPACE => true,
-            self::CHAR_COLON => true
+            self::CHAR_COLON => true,
+            self::CHAR_RIGHT_SQUARE_BRACKET => true
         );
         return $this->fetchName($end_char_arr, true, true);
+    }
+
+    /**
+     * 解析数字
+     * @return string
+     * @throws TplException
+     */
+    private function parseNumber()
+    {
+        $re_str = '';
+        $allow_dot = true;
+        $tmp_index = 0;
+        $allow_hex = false;
+        $is_hex = false;
+        while (!$this->is_eof) {
+            $tmp_index++;
+            $char = $this->indexChar();
+            $ord = ord($char);
+            //第一位是 0,
+            if (1 === $tmp_index && 48 === $ord) {
+                $allow_hex = true;
+                //第二位是 x或者X
+            } elseif ($allow_hex && 2 === $tmp_index && (120 === $ord || 88 === $ord)) {
+                $is_hex = true;
+                $re_str .= $this->popChar();
+                continue;
+            }
+            //遇到.号
+            if (self::CHAR_DOT === $ord) {
+                if ($is_hex || !$allow_dot) {
+                    throw new TplException('小数点出错', TplException::TPL_TAG_ERROR);
+                }
+                $allow_dot = false;
+                $allow_hex = false;
+                $re_str .= $this->popChar();
+                continue;
+            }
+            //遇到空格或者关键字符中止
+            if (isset(self::$key_char_arr[$ord]) || self::CHAR_SPACE === $ord) {
+                break;
+            }
+            if (!$this->isNumber($ord, $is_hex)) {
+                throw new TplException('数字 ' . $re_str . $this->popChar() . ' 解析出错', TplException::TPL_TAG_ERROR);
+            }
+            $re_str .= $this->popChar();
+        }
+        return $re_str;
     }
 
     /**
@@ -226,7 +405,7 @@ class TagParser
      * @return string
      * @throws TplException
      */
-    private function parseNormal($quote_type)
+    private function parseString($quote_type)
     {
         $in_escape = false;
         $is_eof = false;
@@ -240,6 +419,7 @@ class TagParser
                 $in_escape = false;
             } //结束了
             elseif ($ord === $quote_type) {
+                $re_str .= $char;
                 $is_eof = true;
                 break;
             } //转义符
@@ -251,9 +431,9 @@ class TagParser
         }
         //解析字符串未完成， 就结束了
         if (!$is_eof) {
-            throw new TplException('解析出错', TplException::TPL_TAG_ERROR);
+            throw new TplException('字符串 ' . $re_str . ' 出错', TplException::TPL_TAG_ERROR);
         }
-        return $re_str . chr($quote_type);
+        return $re_str;
     }
 
     /**
@@ -268,8 +448,8 @@ class TagParser
         static $end_char_arr = array(
             self::CHAR_SPACE => true,
             self::CHAR_DOT => true,
-            self::CHAR_RIGHT_BRACKET => true,
-            self::CHAR_LEFT_BRACKET => true,
+            self::CHAR_RIGHT_SQUARE_BRACKET => true,
+            self::CHAR_LEFT_SQUARE_BRACKET => true,
             self::CHAR_FILTER => true
         );
         $name = $this->fetchName($end_char_arr, true);
@@ -282,7 +462,7 @@ class TagParser
                 case self::CHAR_DOT:
                     $re_str .= $this->parseDot();
                     break;
-                case self::CHAR_LEFT_BRACKET:
+                case self::CHAR_LEFT_SQUARE_BRACKET:
                     $re_str .= $this->parseBracket();
                     break;
                 default:
@@ -296,9 +476,9 @@ class TagParser
             $filter = $this->parseFilter();
             print_r($filter);
             $this->trim();
-            $re_str = 'modifier_'. $filter['name'] . '('. $re_str;
+            $re_str = 'modifier_' . $filter['name'] . '(' . $re_str;
             if (!empty($filter['args'])) {
-                $re_str .= ', '. join(', ', $filter['args']);
+                $re_str .= ', ' . join(', ', $filter['args']);
             }
             $re_str .= ')';
         }
@@ -346,25 +526,16 @@ class TagParser
      */
     private function parseBracket()
     {
+        //弹出[
+        $this->popChar();
         $re_str = '[';
-        $this->index++;
-        $first_char = $this->popChar();
-        $ord = ord($first_char);
-        //单引号 或者 双引号
-        if (self::CHAR_QUOTE === $ord || self::CHAR_SINGLE_QUOTE === $ord) {
-            $re_str .= $first_char;
-            $re_str .= $this->parseNormal($ord);
-            $re_str .= $first_char;
-        } //变量
-        elseif ('$' === $first_char) {
-            $re_str .= $this->parseVar();
-        }
+        $re_str .= $this->parseValue();
         $this->trim();
         if (']' !== $this->popChar(false)) {
             throw new TplException('中括号不配对', TplException::TPL_TAG_ERROR);
         }
         $re_str .= ']';
-        $this->break_char = self::CHAR_RIGHT_BRACKET;
+        $this->break_char = self::CHAR_RIGHT_SQUARE_BRACKET;
         return $re_str;
     }
 
@@ -379,7 +550,7 @@ class TagParser
         $re_str = "['";
         $end_char_arr = array(
             self::CHAR_SPACE => true,
-            self::CHAR_LEFT_BRACKET => true,
+            self::CHAR_LEFT_SQUARE_BRACKET => true,
             self::CHAR_DOT => true,
             self::CHAR_FILTER => true
         );
@@ -400,14 +571,39 @@ class TagParser
     private function validNameCheck($ord, $allow_int)
     {
         //如果是数字 子游标不能是0
-        $is_int = $ord >= 48 && $ord <= 57;
+        $is_int = $this->isNumber($ord);
         if ($is_int && !$allow_int) {
             throw new TplException('数字不能出现在关键字第一位', TplException::TPL_TAG_ERROR);
         }
-        //[a-zA-Z_]
-        if (!($ord >= 65 && $ord <= 90) && !($ord >= 97 && $ord <= 122) && $ord !== 95 && !$is_int) {
+        //a-zA-z_和数字
+        if (!$this->isNormalChar($ord) && !$is_int) {
             throw new TplException('关键字只允许a-zA-Z0-9_的字符', TplException::TPL_TAG_ERROR);
         }
+    }
+
+    /**
+     * 是否是普通字符a-zA-Z和_
+     * @param int $ord
+     * @return bool
+     */
+    private function isNormalChar($ord)
+    {
+        return ($ord >= 65 && $ord <= 90)
+        || ($ord >= 97 && $ord <= 122) || 95 === $ord;
+    }
+
+    /**
+     * 是否是数字
+     * @param int $ord
+     * @param bool $is_hex 是否16进制
+     * @return bool
+     */
+    private
+    function isNumber($ord, $is_hex = false)
+    {
+        return ($ord >= 48 && $ord <= 57)
+        || ($is_hex && $ord >= 97 && $ord <= 102)
+        || ($is_hex && $ord >= 65 && $ord <= 70);
     }
 
     /**
@@ -434,6 +630,10 @@ class TagParser
         while (!$this->is_eof) {
             $char = $this->indexChar();
             $ord = ord($char);
+            if (isset(self::$operator_char[$char])) {
+                $this->break_char = $ord;
+                break;
+            }
             if (($is_arr && isset($end_char[$ord])) || (!$is_arr && $ord === $ord)) {
                 $this->break_char = $ord;
                 break;
@@ -464,7 +664,7 @@ class TagParser
      */
     private function popChar($is_throw_exception = true)
     {
-        if ($this->is_eof){
+        if ($this->is_eof) {
             if ($is_throw_exception) {
                 throw new TplException('解析出错', TplException::TPL_TAG_ERROR);
             }
@@ -490,18 +690,11 @@ class TagParser
     }
 
     /**
-     * 返回标签类型
-     * @return int
-     */
-    public function getTagType(){
-        return $this->tag_type;
-    }
-
-    /**
      * 返回属性部分
      * @return array
      */
-    public function getArgs(){
+    public function getArgs()
+    {
         return $this->attribute;
     }
 
--- test/CompilerTestCase.php
+++ test/CompilerTestCase.php
@@ -1,6 +1,6 @@
 <?php
 namespace ffan\php\tpl;
 require_once '../vendor/autoload.php';
-$str = '$test.aaa.bbb.ccc|ddd|eeee:0:$bb';
+$str = '$test.aaa.bbb.ccc|ddd|eeee:0:$aa|bb:$mm[aa] 0Xff000 if (isset($a))';
 $tag_parser = new TagParser($str);
 echo $tag_parser->getResult(), PHP_EOL;
